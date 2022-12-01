b8f6054719870c66439382eaf9a415310c1792a1
Sebastian <sebaas.tiang10@gmail.com>
2022-12-01 14:22:35 -0500

    Funcionalidad para envio de SMS

--- servidor.py
+++ servidor.py
@@ -3,7 +3,7 @@ import os
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 from flask import Flask, request
-# from twilio.rest import Client
+from twilio.rest import Client
 
 app = Flask(__name__)
 
@@ -49,17 +49,16 @@ def email():
 def sms():
     hash = request.form['hash_validator']
     if (hash == os.environ.get('HASH_VALIDATOR')):
+
         try:
             account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
             auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
             client = Client(account_sid, auth_token)
             message = client.messages.create(
-                messaging_service_sid=os.environ.get(
-                    'TWILIO_MESSAGING_SERVICE_SID'),
+                messaging_service_sid=os.environ.get('TWILIO_MESSAGING_SERVICE_SID'),
                 body=request.form['message'],
                 to=request.form['destination']
             )
-            print(message.sid)
             return "Enviado"
         except:
             return "Error: SMS no enviado."
